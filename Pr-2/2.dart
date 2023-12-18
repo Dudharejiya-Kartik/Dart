a3a898e94102a48e95d6230cbf29a8ac6f20e08e
Dudharejiya Kartik <137186473+Dudharejiya-Kartik@users.noreply.github.com>
2023-12-18 13:43:18 +0530

    Update 2.dart

--- Pr-2/2.dart
+++ Pr-2/2.dart
@@ -1,52 +1,90 @@
 import 'dart:io';
-class Supermarket{
-  late int customer_id;
-  late String customer_name;
-  late int customer_number;
 
-  void setCustomerData(){
-    stdout.write("Enter Customer Id:");
-    customer_id = int.parse(stdin.readLineSync()!);
+class Product {
+ int pro_id;
+ String pro_name;
+ int pro_price;
+ String cat_name;
 
-    stdout.write("Enter Customer Name:");
-    customer_name = stdin.readLineSync()!;
-
-    stdout.write("Enter Customer Number:");
-    customer_number = int.parse(stdin.readLineSync()!);
-  }
-
-  void getCustomerData(){
-    print("\nCustomer Id\t: $customer_id");
-    print("Customer Name\t: $customer_name");
-    print("Customer Number\t: $customer_number");
-  }
-
+ Product({required this.pro_id, required this.pro_name, required this.pro_price, required this.cat_name});
 }
 
-class product{
-  late int pro_id;
-  late String pro_name;
-  late int quantity;
-  late int price;
+class Customer {
+ int cust_id;
+ String cust_name;
+ String cust_contact;
+ List<Map<String, dynamic>> cust_cart;
 
+ Customer({required this.cust_id, required this.cust_name, required this.cust_contact, required this.cust_cart});
+
+ void add_to_cart(Product product) {
+   cust_cart.add({'pro_id': product.pro_id, 'pro_name': product.pro_name, 'pro_qty': 1, 'pro_price': product.pro_price});
+ }
+
+ double calculate_bill() {
+   double total = 0;
+   cust_cart.forEach((item) {
+     total += item['pro_qty'] * item['pro_price'];
+   });
+
+   if (total >= 500 && total <= 1500) {
+     total -= total * 0.1;
+   } else if (total > 1500 && total <= 3500) {
+     total -= total * 0.2;
+   } else if (total > 3500 && total <= 6500) {
+     total -= total * 0.25;
+   } else if (total > 6500) {
+     total -= total * 0.3;
+   }
+
+   return total;
+ }
+
+ void search_customer(int cust_id) {
+   if (cust_cart.isNotEmpty) {
+     cust_cart.forEach((item) {
+       stdout.write('Product ID: ${item['pro_id']}, Product Name: ${item['pro_name']}, Product Quantity: ${item['pro_qty']}, Product Price: ${item['pro_price']}\n');
+     });
+     stdout.writeln('Total Bill: ${calculate_bill()}');
+   } else {
+     stdout.writeln('No products in cart');
+   }
+ }
 }
 
-void main(){
-  int n;
+class SuperMarket {
+ List<Customer> customers;
 
-  stdout.write("Enter Number Of Customer:");
-  n = int.parse(stdin.readLineSync()!);
+ SuperMarket({required this.customers});
 
-  List obj =List.generate(n, (index) => Supermarket());
-  obj.forEach((e) {
-    e.setCustomerData();
-  });
+ void add_customer(Customer customer) {
+   customers.add(customer);
+ }
 
-  obj.forEach((e) {
-    e.getCustomerData();
-  });
+ void add_product_to_cart(int cust_id, Product product) {
+   Customer? customer = customers.firstWhere((cust) => cust.cust_id == cust_id, orElse: () => null);
+   if (customer != null) {
+     customer.add_to_cart(product);
+   } else {
+     stdout.writeln('Customer not found');
+   }
+ }
+}
 
+void main() {
+ SuperMarket superMarket = SuperMarket(customers: []); 
 
+ superMarket.add_customer(Customer(cust_id: 1, cust_name: 'Kartik', cust_contact: '1234567890', cust_cart: []));
 
+ List<Product> products = [
+    Product(pro_id: 1, pro_name: 'Product 1', pro_price: 500, cat_name: 'Category 1'),
+    Product(pro_id: 2, pro_name: 'Product 2', pro_price: 1000, cat_name: 'Category 2'),
+    Product(pro_id: 3, pro_name: 'Product 3', pro_price: 3000, cat_name: 'Category 3'),
+ ];
 
+ superMarket.add_product_to_cart(1, products[0]);
+ superMarket.add_product_to_cart(1, products[1]);
+ superMarket.add_product_to_cart(1, products[2]);
+
+ superMarket.customers[0].search_customer(1);
 }
